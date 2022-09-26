172c062f4874a7200ccd165332d0b91878677620
Haadi Razzak <hrazzak@usc.edu>
2022-09-26 21:22:53 +0000

    Commit for checkpoint

--- clothing.h
+++ clothing.h
@@ -1,7 +1,6 @@
 #ifndef CLOTHING_H
 #define CLOTHING_H
 #include "product.h"
-using namespace std;
 
 class Clothing : public Product {
 public:
--- movie.h
+++ movie.h
@@ -1,7 +1,6 @@
 #ifndef MOVIE_H
 #define MOVIE_H
 #include "product.h"
-using namespace std;
 
 class Movie : public Product {
 public:
